56239134025688481f745c8d42f3ea872f690d34
bpiper2 <pbrent135@gmail.com>
2025-03-20 15:00:09 -0400

    Add HelloWorld.java to demonstrate package structure

---
+++ Spring_25/src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.question1;
+
+/**
+ * A simple Hello World program to demonstrate package structure
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+} 
