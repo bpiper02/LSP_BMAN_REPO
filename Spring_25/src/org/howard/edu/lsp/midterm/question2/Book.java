681b8ea0f816dfc0f3bd5712d3c0478da91fdfd5
bpiper2 <pbrent135@gmail.com>
2025-03-20 19:47:37 -0400

    Answer for question 2

---
+++ Spring_25/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -0,0 +1,73 @@
+package org.howard.edu.lsp.midterm.question2;
+
+/**
+ * Represents a book in a library with title, author, ISBN, and publication year.
+ */
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private int yearPublished;
+
+    /**
+     * Constructor for creating a new Book
+     */
+    public Book(String title, String author, String ISBN, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.yearPublished = yearPublished;
+    }
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    /**
+     * Overridden equals method to compare books based on ISBN and author
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book other = (Book) obj;
+        return ISBN.equals(other.ISBN) && author.equals(other.author);
+    }
+
+    /**
+     * Overridden toString method to provide a string representation of the book
+     */
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
+    }
+} 
