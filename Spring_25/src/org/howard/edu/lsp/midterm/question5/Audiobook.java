de92ba82a2892eff372a447dcf2a53a3ce43be68
bpiper2 <pbrent135@gmail.com>
2025-03-20 20:06:37 -0400

    Answer to question 5

---
+++ Spring_25/src/org/howard/edu/lsp/midterm/question5/Audiobook.java
@@ -0,0 +1,31 @@
+package org.howard.edu.lsp.midterm.question5;
+
+/**
+ * Represents audiobook content in the streaming service
+ */
+public class Audiobook implements Streamable {
+    private String title;
+
+    public Audiobook(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing audiobook: " + title);
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("Paused audiobook: " + title);
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopped audiobook: " + title);
+    }
+
+    public void setPlaybackSpeed(double speed) {
+        System.out.println("Setting playback speed of audiobook: " + title + " " + speed + "x");
+    }
+} 
---
+++ Spring_25/src/org/howard/edu/lsp/midterm/question5/Streamable.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.question5;
+
+/**
+ * Interface for streamable content
+ */
+public interface Streamable {
+    void play();
+    void pause();
+    void stop();
+} 
